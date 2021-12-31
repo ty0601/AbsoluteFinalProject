97a413af392b20b151dc416d0373a4f9547bd26c
ty0601 <tommy00660011@gmail.com>
2021-12-31 16:18:39 +0800

    commit

--- app.py
+++ app.py
@@ -12,94 +12,114 @@ from utils import send_text_message
 
 load_dotenv()
 
-
 machine = TocMachine(
-    states=["user", "play1", "play2", "play3", "play4", "read1", "read2", "read3", "read4"],
+    states=["user", "state1", "state2"],
     transitions=[
         {
             "trigger": "advance",
             "source": "user",
-            "dest": "play1",
-            "conditions": "is_going_to_play",
-        },
-        {
-            "trigger": "advance",
-            "source": "play1",
-            "dest": "play2",
-            "conditions": "is_going_to_play",
-        },
-        {
-            "trigger": "advance",
-            "source": "play2",
-            "dest": "play3",
-            "conditions": "is_going_to_play",
-        },
-        {
-            "trigger": "advance",
-            "source": "play3",
-            "dest": "play4",
-            "conditions": "is_going_to_play",
-        },
-        {
-            "trigger": "advance",
-            "source": "play4",
-            "dest": "play4",
-            "conditions": "is_going_to_play",
+            "dest": "state1",
+            "conditions": "is_going_to_state1",
         },
         {
             "trigger": "advance",
             "source": "user",
-            "dest": "read1",
-            "conditions": "is_going_to_read1",
+            "dest": "state2",
+            "conditions": "is_going_to_state2",
         },
-        {
-            "trigger": "advance",
-            "source": "read1",
-            "dest": "read2",
-            "conditions": "is_going_to_read",
-        },
-        {
-            "trigger": "advance",
-            "source": "read2",
-            "dest": "read3",
-            "conditions": "is_going_to_read",
-        },
-        {
-            "trigger": "advance",
-            "source": "read3",
-            "dest": "read4",
-            "conditions": "is_going_to_read",
-        },
-        {
-            "trigger": "advance",
-            "source": "read4",
-            "dest": "read4",
-            "conditions": "is_going_to_read",
-        },
-        {
-            "trigger": "advance",
-            "source": ["user", "play1", "play2", "play3", "play4", "read1", "read2", "read3", "read4"],
-            "dest": "user",
-            "conditions": "is_going_to_reset",
-        },
-        {
-            "trigger": "advance",
-            "source": ["play1", "play2", "play3", "play4"],
-            "dest": "read1",
-            "conditions": "is_going_to_read",
-        },
-        {
-            "trigger": "advance",
-            "source": ["read1", "read2", "read3", "read4"],
-            "dest": "play1",
-            "conditions": "is_going_to_play",
-        },
-
+        {"trigger": "go_back", "source": ["state1", "state2"], "dest": "user"},
     ],
     initial="user",
     auto_transitions=False,
     show_conditions=True,
 )
+# machine = TocMachine(
+#     states=["user", "play1", "play2", "play3", "play4", "read1", "read2", "read3", "read4"],
+#     transitions=[
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "play1",
+#             "conditions": "is_going_to_play",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "play1",
+#             "dest": "play2",
+#             "conditions": "is_going_to_play",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "play2",
+#             "dest": "play3",
+#             "conditions": "is_going_to_play",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "play3",
+#             "dest": "play4",
+#             "conditions": "is_going_to_play",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "play4",
+#             "dest": "play4",
+#             "conditions": "is_going_to_play",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "user",
+#             "dest": "read1",
+#             "conditions": "is_going_to_read",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "read1",
+#             "dest": "read2",
+#             "conditions": "is_going_to_read",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "read2",
+#             "dest": "read3",
+#             "conditions": "is_going_to_read",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "read3",
+#             "dest": "read4",
+#             "conditions": "is_going_to_read",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": "read4",
+#             "dest": "read4",
+#             "conditions": "is_going_to_read",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": ["user", "play1", "play2", "play3", "play4", "read1", "read2", "read3", "read4"],
+#             "dest": "user",
+#             "conditions": "is_going_to_reset",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": ["play1", "play2", "play3", "play4"],
+#             "dest": "read1",
+#             "conditions": "is_going_to_read",
+#         },
+#         {
+#             "trigger": "advance",
+#             "source": ["read1", "read2", "read3", "read4"],
+#             "dest": "play1",
+#             "conditions": "is_going_to_play",
+#         },
+#
+#     ],
+#     initial="user",
+#     auto_transitions=False,
+#     show_conditions=True,
+# )
 
 app = Flask(__name__, static_url_path="")
 
@@ -118,31 +138,31 @@ line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
 
-@app.route("/callback", methods=["POST"])
-def callback():
-    signature = request.headers["X-Line-Signature"]
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # parse webhook body
-    try:
-        events = parser.parse(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
-        )
-
-    return "OK"
+# @app.route("/callback", methods=["POST"])
+# def callback():
+#     signature = request.headers["X-Line-Signature"]
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#
+#     # parse webhook body
+#     try:
+#         events = parser.parse(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+#
+#     # if event is MessageEvent and message is TextMessage, then echo text
+#     for event in events:
+#         if not isinstance(event, MessageEvent):
+#             continue
+#         if not isinstance(event.message, TextMessage):
+#             continue
+#
+#         line_bot_api.reply_message(
+#             event.reply_token, TextSendMessage(text=event.message.text)
+#         )
+#
+#     return "OK"
 
 
 @app.route("/webhook", methods=["POST"])
--- fsm.py
+++ fsm.py
@@ -3,70 +3,97 @@ from transitions.extensions import GraphMachine
 from utils import send_text_message
 
 
+# class TocMachine(GraphMachine):
+#     def __init__(self, **machine_configs):
+#         self.machine = GraphMachine(model=self, **machine_configs)
+#
+#     def is_going_to_read(self, event):
+#         text = event.message.text
+#         return text.lower() == "study"
+#
+#     def is_going_to_play(self, event):
+#         text = event.message.text
+#         return text.lower() == "play"
+#
+#     def is_going_to_reset(self, event):
+#         text = event.message.text
+#         return text.lower() == "reset"
+#
+#     def on_enter_to_read1(self, event):
+#         print("I'm reading")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Study for two hours.")
+#
+#     def on_enter_to_read2(self, event):
+#         print("I'm reading")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Study for two hours.")
+#
+#     def on_enter_to_read3(self, event):
+#         print("I'm reading")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Study for two hours.")
+#
+#     def on_enter_to_read4(self, event):
+#         print("I'm reading")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "You need to take a break!")
+#
+#     def on_enter_to_play1(self, event):
+#         print("I'm playing")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Play for two hours.")
+#
+#     def on_enter_to_play2(self, event):
+#         print("I'm playing")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Play for two hours.")
+#
+#     def on_enter_to_play3(self, event):
+#         print("I'm playing")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Play for two hours.")
+#
+#     def on_enter_to_play4(self, event):
+#         print("I'm playing")
+#
+#         reply_token = event.reply_token
+#         send_text_message(reply_token, "Don't play anymore!")
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
         self.machine = GraphMachine(model=self, **machine_configs)
 
-    def is_going_to_read(self, event):
+    def is_going_to_state1(self, event):
         text = event.message.text
-        return text.lower() == "study"
+        return text.lower() == "go to state1"
 
-    def is_going_to_read1(self, event):
+    def is_going_to_state2(self, event):
         text = event.message.text
-        return text.lower() == "study1"
+        return text.lower() == "go to state2"
 
-    def is_going_to_play(self, event):
-        text = event.message.text
-        return text.lower() == "play"
-
-    def is_going_to_reset(self, event):
-        text = event.message.text
-        return text.lower() == "reset"
-
-    def on_enter_to_read1(self, event):
-        print("I'm reading")
+    def on_enter_state1(self, event):
+        print("I'm entering state1")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Study for two hours.")
+        send_text_message(reply_token, "Trigger state1")
+        self.go_back()
 
-    def on_enter_to_read2(self, event):
-        print("I'm reading")
+    def on_exit_state1(self):
+        print("Leaving state1")
 
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Study for two hours.")
-
-    def on_enter_to_read3(self, event):
-        print("I'm reading")
+    def on_enter_state2(self, event):
+        print("I'm entering state2")
 
         reply_token = event.reply_token
-        send_text_message(reply_token, "Study for two hours.")
+        send_text_message(reply_token, "Trigger state2")
+        self.go_back()
 
-    def on_enter_to_read4(self, event):
-        print("I'm reading")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "You need to take a break!")
-
-    def on_enter_to_play1(self, event):
-        print("I'm playing")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Play for two hours.")
-
-    def on_enter_to_play2(self, event):
-        print("I'm playing")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Play for two hours.")
-
-    def on_enter_to_play3(self, event):
-        print("I'm playing")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Play for two hours.")
-
-    def on_enter_to_play4(self, event):
-        print("I'm playing")
-
-        reply_token = event.reply_token
-        send_text_message(reply_token, "Don't play anymore!")
+    def on_exit_state2(self):
+        print("Leaving state2")
